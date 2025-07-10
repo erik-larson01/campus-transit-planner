f72d790a515ff6474feb8ac7905a04858399d691
Erik Larson <erikmlarson5@gmail.com>
2025-07-10 17:03:05 -0400

    buildings.py loading and filtering

--- core/buildings.py
+++ core/buildings.py
@@ -1,21 +1,38 @@
 import json
+from typing import List, Dict, Any
+from rapidfuzz import process, fuzz
 
-def load_buildings(json_path: str):
+def load_buildings(json_path: str = "data/buildings.geojson") -> List[Dict[str]]:
     """
     Load campus buildings data from JSON/GeoJSON.
     :param json_path: path to buildings JSON file
-    :return: dict of building info with coordinates
+    :return: list of building info dicts with name, address, and coordinates
     """
-    pass
+    with open(json_path, "r") as f:
+        data = json.load(f)
 
-def get_building_coordinates(building_name: str, buildings_data: dict):
-    """
-    Retrieve lat/lng coordinates for a given building.
-    :param building_name: name or ID of building
-    :param buildings_data: dict of loaded building info
-    :return:dict of a buildings name and coordinates
-    """
-    #TODO return a dict of name and latlng for every building
-    #TODO handle names that do not exactly match the data
+    updated_building_info = get_building_info(data)
+    return updated_building_info
 
-    pass
+def get_building_info(buildings_data: List[Dict[str]]) -> List[Dict[str]]:
+    """
+    Extracts a list of a buildings geo info, such as name, street address and coordinates
+    :param buildings_data: list of all buildings from loaded buildings.geojson
+    :return: list of dicts with keys: name, street_address, lat, lon
+    """
+    results = []
+    for building in buildings_data:
+        name = building.get("name")
+        street_address = building.get("street_address")
+        latlng = building.get("latlng", None)
+        if latlng:
+            lat, lon = latlng[0], latlng[1]
+        else:
+            lat, lon = None, None
+        results.append({
+            "name": name,
+            "street_address": street_address,
+            "lat": lat,
+            "long": lon
+        })
+    return results
