9e548b7207b024b29467c995afedfb24acd8fdc4
Erik Larson <erikmlarson5@gmail.com>
2025-07-11 16:08:09 -0400

    finished schedule parsing logic and data flow

--- core/schedule_parser.py
+++ core/schedule_parser.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Any
 from utils.time_utils import is_time_before
+
 import pandas as pd
 
 def load_schedule(csv_path: str = "data/class_schedule.txt") -> List[Dict[str, Any]]:
@@ -9,7 +10,10 @@ def load_schedule(csv_path: str = "data/class_schedule.txt") -> List[Dict[str, A
     :return: list of class dicts with time, building, day info
     """
     required_columns = ["course_code", "class_name", "day", "start_time", "end_time", "building"]
-    df = pd.read_csv(csv_path)
+    try:
+        df = pd.read_csv(csv_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Schedule file not found: {csv_path}")
     for col in required_columns:
         if col not in df.columns:
             raise ValueError(f"Missing required column: '{col}' in class_schedule.txt.")
@@ -82,3 +86,49 @@ def validate_schedule_data(schedule_data: List[Dict[str, Any]]) -> List[Dict[str
         validated.append(item)
 
     return validated
+
+def extract_unique_building_names(schedule_data: List[Dict[str, Any]]) -> List[str]:
+    """
+    Extracts a list of unique building names from the schedule data.
+
+    :param schedule_data: list of class records from validated schedule
+    :return: list of unique building names
+    """
+    result = []
+
+    for item in schedule_data:
+        building = item.get("building")
+        already_in_result = False
+
+        for existing in result:
+            if building == existing:
+                already_in_result = True
+                break
+        if building and not already_in_result:
+            result.append(building)
+
+    return result
+
+
+def apply_matched_building_names(schedule_data: List[Dict[str, Any]],
+                                 name_mapping: Dict[str, str]
+                                 ) -> List[Dict[str, Any]]:
+    """
+    Updates the building field in schedule_data based on correct name mapping to be used for
+    the rest of the program
+
+    :param schedule_data: List of dicts, each representing a class with a building key
+    :param name_mapping: Dict mapping user input building names to official building names in buildings.geojson
+    :return: Updated list of dicts with corrected building names
+    """
+    updated_schedule = []
+
+    for item in schedule_data:
+        original_building = item.get("building")
+        corrected_building = name_mapping.get(original_building)
+
+        updated_item = dict(item)
+        updated_item["building"] = corrected_building
+        updated_schedule.append(updated_item)
+
+    return updated_schedule
