8c67340dd3716fe243e233e6fb8294df6c17fd5c
Erik Larson <erikmlarson5@gmail.com>
2025-07-16 15:21:53 -0400

    refactored config.py to be used in route_planner.py instead

--- core/suggestions.py
+++ core/suggestions.py
@@ -1,9 +1,9 @@
-from utils.config import MAX_WALKING_DISTANCE_METERS
+
+MAX_WALKING_DISTANCE_METERS = 800 #Temp
 def suggest_transport_option(distance: float):
     """
     Suggest whether to walk or take bus based on distance.
     :param distance: distance in meters from origin to destination
-    :param walking_threshold: max distance to prefer walking
     :return: 'walk' or 'bus'
     """
     if distance < MAX_WALKING_DISTANCE_METERS:
--- utils/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
-MAX_WALKING_DISTANCE_METERS = 800 #Approx a 10-minute walk
